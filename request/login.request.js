d6bcbb3a69fdb23603bdffc695677f96f34c6ad1
Ryan Dulko <ryandulko458@gmail.com>
2024-07-07 11:51:14 -0300

    babel

--- request/login.request.js
+++ request/login.request.js
@@ -1,26 +1,27 @@
+import { check } from "k6";
 import http from "k6/http"
 
 export default class login {
    #token
- 
+
    access(user, pass) {
-     let response=http.post(`${Utils.getBaseUrl}/login`, JSON.stringify(
-        {
+      let response = http.post(`${Utils.getBaseUrl}/login`, JSON.stringify(
+         {
             "username": user,
             "password": pass
-        }
-     ), {
-        headers: {
+         }
+      ), {
+         headers: {
             "Content-Type": "application/json",
             "Accept": "application/json"
-        }
-     })
-     this.#token = response.json('accessToken')
-     check(request, {
-        "O status deve ser": (r) => r.status === 201
-    });
+         }
+      })
+      this.#token = response.json('accessToken')
+      check(response, {
+         "O status deve ser": (r) => r.status === 201
+      });
    }
-   getToken(){
-    return this.#token
+   getToken() {
+      return this.#token
    }
 }
